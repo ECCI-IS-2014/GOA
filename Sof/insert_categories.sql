82bce7cf9f01ee3572651b1c3af3a466cc0b4110
David <dav793@gmail.com>
2014-10-09 07:41:35 -0600

    Agregué archivos con sql queries para poblar las tablas categories y products con datos de prueba

---
+++ Sof/insert_categories.sql
@@ -0,0 +1,40 @@
+
+INSERT INTO categories ( id, name )
+                       VALUES
+                       ( 0, 'ropa' );
+
+INSERT INTO categories ( id, name )
+                       VALUES
+                       ( 1, 'higiene personal' );
+
+INSERT INTO categories ( id, name )
+                       VALUES
+                       ( 2, 'alimentos' );
+
+INSERT INTO categories ( id, name )
+                       VALUES
+                       ( 3, 'electrodomesticos' );
+
+INSERT INTO categories ( id, name )
+                       VALUES
+                       ( 4, 'muebles' );
+
+INSERT INTO categories ( id, name )
+                       VALUES
+                       ( 5, 'hogar' );
+
+INSERT INTO categories ( id, name, father_category_id )
+                       VALUES
+                       ( 6, 'sombreros', 0 );
+
+INSERT INTO categories ( id, name, father_category_id )
+                       VALUES
+                       ( 7, 'calzado', 0 );
+
+INSERT INTO categories ( id, name )
+                       VALUES
+                       ( 8, 'limpieza' );
+
+INSERT INTO categories ( id, name )
+                       VALUES
+                       ( 9, 'miscelaneo' );
