3de65704e5ebe04f1ae7abe33a0b114b97bd09d6
unknown <b25528@105-19.ecci.ucr.ac.cr>
2014-10-13 13:03:29 -0600

    Le agregue a las pruebas la prueba del logout, e hice un camnbio enla base de datos, en la tabla users

--- Sof/BD.sql
+++ Sof/BD.sql
@@ -1,9 +1,9 @@
-﻿-- phpMyAdmin SQL Dump
+-- phpMyAdmin SQL Dump
 -- version 4.1.12
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-10-2014 a las 23:04:16
+-- Tiempo de generación: 13-10-2014 a las 21:01:04
 -- Versión del servidor: 5.6.16
 -- Versión de PHP: 5.5.11
 
@@ -23,58 +23,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `categories`
---
-
-CREATE TABLE IF NOT EXISTS `categories` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) NOT NULL,
-  `father_category_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_category_father_id` (`father_category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `products`
---
-
-CREATE TABLE IF NOT EXISTS `products` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `category_id` int(11) NOT NULL,
-  `name` varchar(30) NOT NULL,
-  `price` decimal(9,2) NOT NULL DEFAULT '0.00',
-  `quantity` int(11) NOT NULL DEFAULT '0',
-  `image` varchar(150) DEFAULT NULL,
-  `enable_product` tinyint(1) DEFAULT '1',
-  `rating` int(11) DEFAULT '0',
-  `description` varchar(1000) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_category_id` (`category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `ratings`
---
-
-CREATE TABLE IF NOT EXISTS `ratings` (
-  `product_id` int(11) NOT NULL,
-  `enable_rating` tinyint(1) DEFAULT '1',
-  `rating1` int(11) NOT NULL DEFAULT '0',
-  `rating2` int(11) NOT NULL DEFAULT '0',
-  `rating3` int(11) NOT NULL DEFAULT '0',
-  `rating4` int(11) NOT NULL DEFAULT '0',
-  `rating5` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`product_id`),
-  KEY `FK_product_id` (`product_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `users`
 --
 
@@ -82,7 +30,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(40) NOT NULL,
   `password` varchar(40) NOT NULL,
-  `role` tinyint(1) DEFAULT '0', -- cero será usuario, 1 será el admin
+  `role` tinyint(1) DEFAULT '0',
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
   `name` varchar(30) NOT NULL,
@@ -92,31 +40,17 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(40) NOT NULL,
   `gender` char(1) NOT NULL,
   `birth_date` date NOT NULL,
-  `status` tinyint(1) DEFAULT '0',  -- habilitado(0) o deshabilidado (1)
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `status` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
--- Restricciones para tablas volcadas
+-- Volcado de datos para la tabla `users`
 --
 
---
--- Filtros para la tabla `categories`
---
-ALTER TABLE `categories`
-  ADD CONSTRAINT `FK_category_father_id` FOREIGN KEY (`father_category_id`) REFERENCES `categories` (`id`);
-
---
--- Filtros para la tabla `products`
---
-ALTER TABLE `products`
-  ADD CONSTRAINT `FK_category_id` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
-  
---
--- Filtros para la tabla `ratings`
---
-ALTER TABLE `ratings`
-  ADD CONSTRAINT `FK_product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+INSERT INTO `users` (`id`, `username`, `password`, `role`, `created`, `modified`, `name`, `last_name`, `phone`, `address`, `email`, `gender`, `birth_date`, `status`) VALUES
+(1, 'Horse', '$2a$10$uOHQuZ.gd1CT7ESHdqMs9O22N0sy7CbWo', 0, '2014-10-13 20:58:35', '2014-10-13 20:58:35', 'Julio', 'Fraile', 85967412, 'Heredia', 'juan@gmail.com', 'M', '1996-10-13', 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
