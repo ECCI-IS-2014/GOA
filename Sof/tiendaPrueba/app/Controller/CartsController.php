c5224ff5ba75bc7630c550de29f7a5a8ec3aaaf6
Carolina <caro.cvs@gmail.com>
2014-10-27 10:06:52 -0600

    Avance en cart

--- Sof/tiendaPrueba/app/Controller/CartsController.php
+++ Sof/tiendaPrueba/app/Controller/CartsController.php
@@ -1,6 +1,5 @@
 <?php
 App::uses('AppController', 'Controller');
-session_start();
 
 class CartsController extends AppController{
 
@@ -12,9 +11,9 @@ class CartsController extends AppController{
     public function index() {
         $prodCarts = array();
 
-       foreach($_SESSION as $valor){
+        foreach($_SESSION as $valor){
            array_push($prodCarts,$valor);
-       }
+        }
         unset($valor);
 
         $this->set('prodCarts',$prodCarts);
@@ -27,22 +26,24 @@ class CartsController extends AppController{
      * @return void
      */
     public function add() {
-        $p_id = $this->passedArgs['id'];
+        $prod_id = $this->passedArgs['id'];
+
         try {
-            if ($this->Session->$_SESSION[$p_id]=$p_id) {
+            if ($this->Session->$_SESSION[$prod_id] = $prod_id) {
                 $this->redirect(
-                    array('controller' => 'Products', 'action' => 'productInside','id'=>$p_id)
+                    array('controller' => 'Products', 'action' => 'productInside','id'=>$prod_id)
                 );
             } else {
                 $this->Session->setFlash(__('The product could not be saved in your Cart. Please, try again.'));
             }
         } catch (Exception $e) {
             $this->Session->setFlash(__('This item was already in your Cart. If you want to increment product quantity please edit your Cart.'));
-            return $this->redirect(array('controller' => 'Products', 'action' => 'productInside','id'=>$p_id));
+            return $this->redirect(array('controller' => 'Products', 'action' => 'productInside','id'=>$prod_id));
 
         }
 
     }
+
 }
 
 ?>
