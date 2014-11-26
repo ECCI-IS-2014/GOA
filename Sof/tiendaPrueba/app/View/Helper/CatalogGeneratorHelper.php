12608079c573d99dcec02ccbb63d826d712e49b6
unknown <b16844@105-16.ecci.ucr.ac.cr>
2014-11-26 09:06:07 -0600

    Resolví bug con slider de home que no permitía abrir los productos

--- Sof/tiendaPrueba/app/View/Helper/CatalogGeneratorHelper.php
+++ Sof/tiendaPrueba/app/View/Helper/CatalogGeneratorHelper.php
@@ -12,7 +12,7 @@ class CatalogGeneratorHelper extends AppHelper {
 
     	echo $this->Html->css('catalogs');
 
-    	$result_string = '<div type="catalog" style="z-index: -10;">';
+    	$result_string = '<div type="catalog">';
 
         for($i = 0; $i < count($products); $i++) {
 
